6a0f99ca9520fbbdc1f9d275e69c47ffe2a4e48d
SandroIerna <iernasandro@gmail.com>
2022-11-11 16:58:01 +0100

    adds solution to the project, with minor issues

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const headers = {
+  "Content-Type": "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
+};
